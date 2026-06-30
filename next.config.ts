f77c5a978075f539e99f2fa92b7305fa65d118bc
Zilton abdul <ziltontuaireabdulj@gmail.com>
2026-06-30 11:19:38 +0200

    Ativa otimização de CSS crítico para reduzir bloqueio de renderização

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    optimizeCss: true,
+  },
   images: {
     deviceSizes: [360, 640, 768, 1024, 1280, 1600, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
